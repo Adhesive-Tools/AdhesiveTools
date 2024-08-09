aa034b97c4d5db00bf8931bdf6c218d6e791263e
Adhesive-Tools <r223moha@gmail.com>
2024-08-09 05:14:09 +0200

    Delete Adhesive - TOOLS directory

--- Adhesive - TOOLS/readme.txt
+++
@@ -1,2 +0,0 @@
-Option grabber create une fois creer aller dans l'emplacement si dessous et retrouver votre grab
-utils main tools dist
--- Adhesive - TOOLS/setup.bat
+++
@@ -1,7 +0,0 @@
-@echo off & title ADHESIVE .gg/jf4ECtdjuG
-
-color 5
-python .\utils\main\tools\setup.py
-pause
-
-pip install pyfiglet
